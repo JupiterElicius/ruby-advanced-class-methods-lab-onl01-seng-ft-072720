f58f98cae35b65c5bf5de029d7d03fea7569bfd3
Nick Bazaldua <bazalnick@gmail.com>
2020-08-09 21:19:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,9 @@ class Song
   end
   
   def self.alphabetical
-    @@all.each.name.sort{ |a,b| a.name b.name }
+    @@all.sort{ |a,b| a.name <=> b.name }
+    
+    
   end
   
   #def self.new_from_filename
