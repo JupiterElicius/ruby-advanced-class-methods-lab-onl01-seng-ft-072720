483ea68f60b779b0e3ca8d71e30fbd9d7862b819
Nick Bazaldua <bazalnick@gmail.com>
2020-08-09 21:35:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   
   def self.new_from_filename
     split_filename = filename.chomp(".mp3").split(" - ")
-    song = song.new
+    song = Song.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
     song
@@ -51,7 +51,7 @@ class Song
   
   def self.create_from_filename
     split_filename = filename.chomp(".mp3").split(" - ")
-    song = song.new
+    song = Song.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
     song.save
